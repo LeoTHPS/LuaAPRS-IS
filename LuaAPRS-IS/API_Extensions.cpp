f2b353c0af4958784fdcca2f9f406f0c45697c3c
Francis Barney <leo633395@gmail.com>
2023-05-29 14:05:21 -0700

    refactor

--- LuaAPRS-IS/API_Extensions.cpp
+++ LuaAPRS-IS/API_Extensions.cpp
@@ -7,8 +7,8 @@
 #include "Extensions/Thread.hpp"
 #include "Extensions/Process.hpp"
 
-#define APRS_IS_API_RegisterExtension(__extension_name__)             RegisterExtension_##__extension_name__(lua)
 #define APRS_IS_API_DEFINE_Extension(__extension_name__, ...) void RegisterExtension_##__extension_name__(AL::Lua54::State& lua) __VA_ARGS__
+#define APRS_IS_API_RegisterExtension(__extension_name__)     RegisterExtension_##__extension_name__(lua)
 
 APRS_IS_API_DEFINE_Extension(Types,
 {
